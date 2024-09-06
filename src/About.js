e023010f895cf23bf7269145eb8e6b3588722a86
kesavansuppaiya <kesavankesu1111@gmail.com>
2024-09-06 17:12:10 +0530

    first commit

---
+++ src/About.js
@@ -0,0 +1,10 @@
+const About = () => {
+  return (
+    <main className='About'>
+        <h2>About</h2>
+        <p style={{marginTop: "1rem"}}>Hi I am Kesavan full stack webdeveloper.</p>
+    </main>
+  )
+}
+
+export default About
--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { Link, Route, Routes, useNavigate } from "react-router-dom";
+import About from "./About";
+import Footer from "./Footer";
+import Header from "./Header";
+import Home from "./Home";
+import Missing from "./Missing";
+import Nav from "./Nav";
+import NewPost from "./NewPost";
+import PostPage from "./PostPage";
+import Post from "./Post";
+import PostLayout from "./PostLayout";
+import EditPost from "./EditPost";
+import { useEffect, useState } from "react";
+import { format } from "date-fns";
+import api from "./api/posts1"
+import useWindowSize from "./hooks/useWindowSize";
+import useAxiosFetch from "./hooks/useAxiosFetch"; 
+import { DataProvider } from "./context/DataContext";
+ 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App"> 
+    <DataProvider>
+     
+          <Header title="KESAVAN" />
+          <Nav 
+          
+          />
+          <Routes>
+            <Route path="/" element = {
+            <Home 
+            
+            />} />
+            
+            <Route path="post"> 
+              <Route index element = {<NewPost
+              
+              />} />
+              <Route path=":id" element={<PostPage />} />
+          </Route>
+          <Route path="/edit/:id" element={<EditPost    />}/>
+              
+              
+          <Route path="about" element = { <About />} />
+            <Route path="*" element={<Missing />} /> 
+          </Routes>
+          <Footer />
+      </DataProvider>
+      
     </div>
   );
 }
